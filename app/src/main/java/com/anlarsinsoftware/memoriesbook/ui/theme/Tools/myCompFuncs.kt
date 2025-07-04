b6100697c0199559c056d5aa817be1a25e4a0707
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-04 13:48:40 +0300

    myCompFuncs.kt created

---
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Tools/myCompFuncs.kt
@@ -0,0 +1,61 @@
+package com.anlarsinsoftware.memoriesbook.ui.theme.Tools
+
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun  myBrush() : Brush {
+    val infiniteTransition = rememberInfiniteTransition(label = "background_gradient_animation")
+    val startColor by infiniteTransition.animateColor(
+        initialValue = Color(0xFF4A148C), targetValue = Color(0xFF0D47A1),
+        animationSpec = infiniteRepeatable(animation = tween(4000), repeatMode = RepeatMode.Reverse), label = ""
+    )
+    val endColor by infiniteTransition.animateColor(
+        initialValue = Color(0xFFAD1457), targetValue = Color(0xFF1565C0),
+        animationSpec = infiniteRepeatable(animation = tween(4000), repeatMode = RepeatMode.Reverse), label = ""
+    )
+    return Brush.verticalGradient(colors = listOf(startColor, endColor))
+}
+@Composable
+fun myText(text: String, fontSize: Int, fontWeight: FontWeight, color: Color = Color.Black) {
+    Text(
+        text = text,
+        fontSize = fontSize.sp,
+        fontWeight = fontWeight,
+        color = color
+    )
+}
+
+@Composable
+fun mySpacer(height: Int) {
+    Spacer(Modifier.height(height.dp))
+}
+
+@Composable
+fun myButton(text: String, isOutLined: Boolean, onClick: () -> Unit) {
+    if (isOutLined) {
+        ElevatedButton(onClick) {
+            Text(text)
+        }
+    } else {
+        Button(onClick) {
+            Text(text)
+        }
+    }
+}
