f292ac7fc5f9f42c87f77ed2df1ea3fe7ffd1bbf
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-05 22:30:38 +0300

    LogIn.kt Google button added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/LogIn.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/LogIn.kt
@@ -2,6 +2,7 @@ package com.anlarsinsoftware.memoriesbook.ui.theme.View
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,17 +26,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.anlarsinsoftware.memoriesbook.R
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myBrush
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myButton
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myImageButton
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.mySpacer
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myText
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myTextField
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.showToast
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -60,8 +70,8 @@ fun LoginScreen(navController: NavController) {
 
             mySpacer(16)
             myTextField(
-                 password,
-                 "Şifre",false
+                password,
+                "Şifre", false
             ) {
                 password = it
             }
@@ -69,19 +79,44 @@ fun LoginScreen(navController: NavController) {
             mySpacer(24)
             myButton(
                 text = "Giriş Yap",
-                 false
+                false
             ) {
                 if (userName.isNotEmpty() && password.isNotEmpty()) {
                     Log.d("LoginAttempt", "Kullanıcı: $userName, Şifre: $password")
-                    //navController.navigate("")
+                    navController.navigate("home_screen")
                 } else {
                     Log.d("LoginAttempt", "Alanlar boş bırakılamaz.")
                 }
             }
+
+            mySpacer(100)
+            Card (Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clickable {
+                    navController.navigate("home_screen")
+                },
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ){
+                Row (Modifier.fillMaxWidth()
+                    .padding(5.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                    ){
+                    val context = LocalContext.current
+                    myImageButton (id= R.drawable.google_ico, imageSize = 50){
+                        showToast(context,"SA",false)
+                    }
+                    Spacer(Modifier.width(20.dp))
+                    Text("Google İle Oturum Aç",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp)
+                }
+
+            }
+
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
