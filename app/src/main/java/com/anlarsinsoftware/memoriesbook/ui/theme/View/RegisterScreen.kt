86229fbf5697f9d7de02952663cecd5ce10f55be
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-05 22:30:16 +0300

    RegisterScreen.kt Google button added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/RegisterScreen.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/RegisterScreen.kt
@@ -2,12 +2,18 @@ package com.anlarsinsoftware.memoriesbook.ui.theme.View
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,14 +22,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.anlarsinsoftware.memoriesbook.R
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myBrush
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myButton
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myImageButton
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.mySpacer
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myTextField
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.showToast
 
 @Composable
 fun RegisterScreen(navController: NavController) {
@@ -79,7 +91,32 @@ fun RegisterScreen(navController: NavController) {
                 }
                 else{
                     Log.d("tagReg","Kayıt Başarılı")
+                    navController.navigate("home_screen")
                 }
+            }
+            mySpacer(100)
+            Card (Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clickable {
+                    navController.navigate("home_screen")
+                },
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ){
+                Row (Modifier.fillMaxWidth()
+                    .padding(5.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ){
+                    val context = LocalContext.current
+                    myImageButton (id= R.drawable.google_ico, imageSize = 50){
+                        showToast(context,"SA",false)
+                    }
+                    Spacer(Modifier.width(20.dp))
+                    Text("Google İle Oturum Aç",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp)
+                }
+
             }
 
         }
