ecbf265c5f94c019232d429dcedd3ddf7b181c09
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-11 00:10:49 +0300

    Design updated (EdgeToEdge)

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/MainActivity.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/MainActivity.kt
@@ -2,16 +2,17 @@ package com.anlarsinsoftware.memoriesbook.ui.theme.View
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Posts
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.AppNavigation
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.MemoriesBookTheme
-import com.anlarsinsoftware.memoriesbook.ui.theme.Util.url1
 
 class MainActivity : ComponentActivity() {
     private val postList=ArrayList<Posts>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MemoriesBookTheme {
                 AppNavigation()
