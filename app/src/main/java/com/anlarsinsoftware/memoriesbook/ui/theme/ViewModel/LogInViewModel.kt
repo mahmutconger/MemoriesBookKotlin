1c66032056a2c709f134c73b5ccc10d4d1efe14b
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-22 00:03:34 +0300

    LogInViewModel.kt updated (updateUserLastSignInAndToken func added)

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/ViewModel/LogInViewModel.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/ViewModel/LogInViewModel.kt
@@ -3,10 +3,12 @@ package com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -32,7 +34,7 @@ class LogInViewModel : ViewModel() {
             _uiState.value = LoginUiState.Loading
             try {
                 val result= auth.signInWithEmailAndPassword(email, password).await()
-                result.user?.let { updateUserLastSignIn(it.uid) }
+                result.user?.let { updateUserLastSignInAndToken(it.uid) }
                 _uiState.value = LoginUiState.Success
 
 
@@ -50,7 +52,7 @@ class LogInViewModel : ViewModel() {
                 val credential = GoogleAuthProvider.getCredential(idToken, null)
                 val result = auth.signInWithCredential(credential).await()
                 // GİRİŞ BAŞARILI, ŞİMDİ ZAMANI GÜNCELLE
-                result.user?.let { updateUserLastSignIn(it.uid) }
+                result.user?.let { updateUserLastSignInAndToken(it.uid) }
 
                 _uiState.value = LoginUiState.Success
             } catch (e: Exception) {
@@ -60,6 +62,25 @@ class LogInViewModel : ViewModel() {
             }
         }
     }
+
+    private suspend fun updateUserLastSignInAndToken(uid: String) {
+        try {
+            // Cihazın en güncel FCM token'ını al
+            val token = Firebase.messaging.token.await()
+
+            // Hem son giriş tarihini hem de token'ı tek bir işlemde güncelle
+            firestore.collection("Users").document(uid)
+                .update(mapOf(
+                    "lastSignIn" to Timestamp.now(),
+                    "fcmToken" to token
+                )).await()
+            Log.d("Login", "User last sign-in and FCM token updated.")
+        } catch (e: Exception) {
+            Log.w("Login", "Could not update user token.", e)
+        }
+    }
+
+
     private suspend fun updateUserLastSignIn(uid: String) {
         try {
             firestore.collection("Users").document(uid)
