b3e0e242b182d44ef7c9dbeb2a04efeac140add1
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-23 23:40:52 +0300

    Formatters.kt created timestamp formatter

---
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Tools/Formatters.kt
@@ -0,0 +1,26 @@
+package com.anlarsinsoftware.memoriesbook.ui.theme.Tools
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+@Composable
+fun rememberFormattedTimestamp(timestamp: Timestamp?): String {
+    return remember(timestamp) {
+        if (timestamp == null) return@remember "Tarih bilgisi yok"
+
+        val messageDate = timestamp.toDate()
+        val messageCal = Calendar.getInstance().apply { time = messageDate }
+        val currentCal = Calendar.getInstance()
+
+        if (messageCal.get(Calendar.YEAR) < currentCal.get(Calendar.YEAR)) {
+            SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(messageDate)
+        }
+        else {
+            SimpleDateFormat("dd MMMM, HH:mm", Locale("tr")).format(messageDate)
+        }
+    }
+}
