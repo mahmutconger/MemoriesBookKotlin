31aef4a605281bef548816213100827e6a8b22c8
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-23 23:43:08 +0300

    HomeScreen.kt updated (likers bottom sheet and fullScreenVideo added)

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/HomeScreen/HomeScreen.kt
@@ -1,9 +1,14 @@
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.AlertDialog
@@ -20,6 +25,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,58 +34,56 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.anlarsinsoftware.memoriesbook.R
-import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Comments
 import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Posts
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.BottomNavigationBar
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.ExpandableText
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myImageButton
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myIconButtonPainter
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.mySpacer
+import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.rememberFormattedTimestamp
 import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.showToast
-import com.anlarsinsoftware.memoriesbook.ui.theme.Util.url1
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.CreatePostScreen.FriendSelectorBottomSheet
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.BottomSheetContent
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.EditPostBottomSheet
-import com.anlarsinsoftware.memoriesbook.ui.theme.View.MessageScreen.FriendItem
+import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.FullScreenVideoPlayer
+import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.HomeLazyItems.LikersItem
+import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.HomeLazyItems.PostItem
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.CommentsViewModel
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.ConnectionsViewModel
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.HomeViewModel
 import com.google.firebase.Firebase
-import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Locale
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navController: NavController,
-    // 1. Parametreler artık çok daha sade: Sadece ViewModel'leri alıyoruz.
     homeViewModel: HomeViewModel,
     commentsViewModel: CommentsViewModel,
     connectionsViewModel: ConnectionsViewModel
 ) {
     val postList by homeViewModel.posts.collectAsState()
-    val commentList by commentsViewModel.comments.collectAsState()
     val likers by homeViewModel.postLikers.collectAsState()
     val friends by connectionsViewModel.friends.collectAsState()
     val followers by connectionsViewModel.followers.collectAsState()
-    val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
+    var fullScreenVideoUrl by remember { mutableStateOf<String?>(null) }
 
     val context = LocalContext.current
     val optionsSheetState = rememberModalBottomSheetState()
@@ -92,9 +96,7 @@ fun HomeScreen(
     var showVisibilitySheet by remember { mutableStateOf(false) }
     val comments by commentsViewModel.comments.collectAsState()
     var showLikersSheet by remember { mutableStateOf(false) }
-    val commentLikersSheetState = rememberModalBottomSheetState()
-    var showCommentLikersSheet by remember { mutableStateOf(false) }
-    val commentLikers by commentsViewModel.commentLikers.collectAsState()
+    val showLikersSheetState = rememberModalBottomSheetState()
 
 
     val currentUser = FirebaseAuth.getInstance().currentUser
@@ -153,6 +155,10 @@ fun HomeScreen(
                         commentsViewModel.fetchCommentsForPost(post.documentId)
                         selectedPost = post
                         scope.launch { commentSheetState.show() }
+                    }, onMediaClick = { clickedPost, startIndex ->
+                        navController.navigate("media_viewer/${clickedPost.documentId}/${startIndex}")
+                    }, onVideoFullScreen = { videoUrl ->
+                        fullScreenVideoUrl = videoUrl
                     }
                 )
             }
@@ -179,6 +185,33 @@ fun HomeScreen(
                 }
             )
         }
+    }
+
+    if(showLikersSheet){
+        selectedPost?.let { posts ->
+            ModalBottomSheet(
+                sheetState = showLikersSheetState,
+                onDismissRequest = {
+                    showLikersSheet = false
+                }
+            ) {
+                LazyColumn(contentPadding = PaddingValues(16.dp)) {
+                    items(items = likers) { liker ->
+                        LikersItem(liker.photoUrl, liker.username)
+                    }
+                }
+            }
+
+        }
+    }
+
+    fullScreenVideoUrl?.let { url ->
+        FullScreenVideoPlayer(
+            videoUrl = url,
+            onDismiss = {
+                fullScreenVideoUrl = null
+            }
+        )
     }
 
     if (optionsSheetState.isVisible) {
@@ -236,7 +269,7 @@ fun HomeScreen(
                                 showToast(context, "Hata: Açıklama güncellenemedi.")
                             }
                         }
-                        showEditPostSheet = false // Kaydettikten sonra kapat
+                        showEditPostSheet = false
                     }
                 )
             }
@@ -287,197 +320,51 @@ fun HomeScreen(
 
                 CommentBottomSheetContent(
                     post = post,
-                    commentList = comments, // comments, ViewModel'den collectAsState ile alınmalı
+                    commentList = comments,
                     onCommentLikeClicked = { likedComment ->
                         commentsViewModel.onCommentLikeClicked(likedComment)
                     },
-                    // YENİ BAĞLANTI
                     onAddCommentClicked = { commentText ->
                         commentsViewModel.addComment(post.documentId, commentText)
                     },
                     onHide = {
                         scope.launch { commentSheetState.hide() }
-                    }, onShowCommentLikers = { commentToShowLikers ->
-                        commentsViewModel.fetchLikersForComment(commentToShowLikers)
-                        scope.launch {
-                            commentSheetState.hide()
-                            showCommentLikersSheet = true
-                        }
                     }
                 )
             }
         }
     }
+}
 
-    if (showCommentLikersSheet) {
-        ModalBottomSheet(
-            sheetState = commentLikersSheetState,
-            onDismissRequest = {
-                showCommentLikersSheet = false
-            }
-        ) {
-            LazyColumn(contentPadding = PaddingValues(16.dp)) {
-                items(items = likers) { liker ->
-                    LikersItem(liker.photoUrl, liker.username)
-                }
-            }
+
+@Composable
+fun VideoPlayer(videoUrl: String?, modifier: Modifier) {
+    if (videoUrl == null) return
+
+    val context = LocalContext.current
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(videoUrl))
+            prepare()
         }
     }
-}
 
-@Composable
-fun LikersItem(photoUrl: String, userName: String) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        AsyncImage(
-            model = photoUrl,
-            contentDescription = "User Image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(max = 50.dp),
-            contentScale = ContentScale.Crop
-        )
-        Text(userName, fontSize = 21.sp, fontWeight = FontWeight.Bold)
-        Image(
-            painter = painterResource(R.drawable.like_selected),
-            contentDescription = "Beğenildi butonu"
-        )
+    DisposableEffect(Unit) {
+        onDispose {
+            exoPlayer.release()
+        }
     }
-}
 
-@Composable
-fun PostItem(
-    post: Posts,
-    onMenuClick: () -> Unit,
-    onLikeClick: () -> Unit,
-    onCommentClick: () -> Unit,
-    onShowLikersClick: () -> Unit
-) {
-    val currentUser = Firebase.auth.currentUser
-    val isLikedByMe by remember(post.likedBy, currentUser) {
-        mutableStateOf(currentUser?.uid in post.likedBy)
-    }
-    Card(
+    AndroidView(
+        factory = {
+            PlayerView(it).apply {
+                player = exoPlayer
+            }
+        },
         modifier = Modifier
-
             .fillMaxWidth()
-
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Row(Modifier.fillMaxWidth()) {
-                Text(text = post.useremail, fontWeight = FontWeight.Bold)
-                Spacer(Modifier.weight(1f))
-                myImageButton(
-                    R.drawable.menu_vertical,
-                    imageSize = 20,
-                    onClick = onMenuClick,
-                    tintColor = MaterialTheme.colorScheme.primary
-                )
-            }
-            val formattedDate = remember(post.date) {
-                val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
-                sdf.format(post.date?.toDate())
-            }
-
-            Text(
-                formattedDate,
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Medium,
-                color = Color.LightGray
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            AsyncImage(
-                model = post.downloadUrl,
-                contentDescription = "Post Image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(max = 400.dp),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-
-            ExpandableText(
-                text = post.comment,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            mySpacer(10)
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    // Gruba tıklanabilir özellik vererek kullanıcı deneyimini iyileştiriyoruz
-                    modifier = Modifier.clickable { onLikeClick() }
-                ) {
-                    val likeIcon =
-                        if (isLikedByMe) R.drawable.like_selected else R.drawable.like_unselected
-                    myImageButton(id = likeIcon, onClick = onLikeClick)
-
-                    if (post.likedBy.isNotEmpty()) {
-                        Text(
-                            text = "${post.likedBy.size} beğeni",
-                            modifier = Modifier
-                                .clickable { onShowLikersClick() }
-                                .padding(start = 4.dp, end = 8.dp),
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 14.sp
-                        )
-                    }
-                }
-
-                Row(
-                    modifier = Modifier.clickable { onCommentClick() }.padding(start = 15.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    myImageButton(
-                        id = R.drawable.comment_icon,
-                        imageSize = 25,
-                        onClick = onCommentClick,
-                        tintColor = MaterialTheme.colorScheme.primary
-                    )
-                    if (post.commentCount > 0) {
-                        Text(
-                            text = "${post.commentCount} yorum",
-                            modifier = Modifier.padding(start = 4.dp),
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 14.sp,
-                            color = MaterialTheme.colorScheme.onSurfaceVariant
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun prev_Home() {
-    val post1 = Posts("mahmutconger@gmail.com", "06.07.2025", "Bu ilk post ", url1, Timestamp.now())
-    val post2 = Posts("user@test.com", "06.07.2025", "Bu da ikinci postum!", url1, Timestamp.now())
-    val postList = arrayListOf(post1, post2)
-    val comment5 = Comments("bgun daha ne yapacağızz", Timestamp.now(), "as Conger", "1", "1")
-    val commentList = arrayListOf(comment5)
-
-    val homeViewModel: HomeViewModel = viewModel()
-    val commentsViewModel: CommentsViewModel = viewModel()
-    val connectionsViewModel: ConnectionsViewModel = viewModel()
-
-    HomeScreen(
-        rememberNavController(),
-        homeViewModel,
-        commentsViewModel,
-        connectionsViewModel
+            .height(250.dp)
     )
 }
+
 
