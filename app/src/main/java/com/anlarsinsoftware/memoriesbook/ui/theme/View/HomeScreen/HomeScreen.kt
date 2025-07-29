2f8ca47ac8a40f7e232c3785f1e1eb3284b48599
Mahmut Can Conger <mahmutconger@gmail.com>
2025-07-29 23:37:59 +0300

    Profile Visitor added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/HomeScreen/HomeScreen.kt
@@ -1,29 +1,17 @@
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -33,31 +21,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import com.anlarsinsoftware.memoriesbook.R
 import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Posts
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.BottomNavigationBar
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.ExpandableText
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.MyScaffold
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.myIconButtonPainter
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.mySpacer
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.rememberFormattedTimestamp
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.showToast
+import com.anlarsinsoftware.memoriesbook.ui.theme.Util.MyHomeScaffold
+import com.anlarsinsoftware.memoriesbook.ui.theme.Util.MyScaffold
+import com.anlarsinsoftware.memoriesbook.ui.theme.Util.showToast
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.CreatePostScreen.FriendSelectorBottomSheet
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.BottomSheetContent
 import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.EditPostBottomSheet
@@ -67,9 +43,8 @@ import com.anlarsinsoftware.memoriesbook.ui.theme.View.HomeScreen.HomeLazyItems.
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.CommentsViewModel
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.ConnectionsViewModel
 import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.HomeViewModel
-import com.google.firebase.Firebase
+import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.ProfileViewModel
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import kotlinx.coroutines.launch
 
 
@@ -79,12 +54,13 @@ fun HomeScreen(
     navController: NavController,
     homeViewModel: HomeViewModel,
     commentsViewModel: CommentsViewModel,
-    connectionsViewModel: ConnectionsViewModel
+    profileViewModel: ProfileViewModel,
+    contentScale: ContentScale
 ) {
     val postList by homeViewModel.posts.collectAsState()
     val likers by homeViewModel.postLikers.collectAsState()
-    val friends by connectionsViewModel.friends.collectAsState()
-    val followers by connectionsViewModel.followers.collectAsState()
+    val friends by profileViewModel.friends.collectAsState()
+    val followers by profileViewModel.followers.collectAsState()
     var fullScreenVideoUrl by remember { mutableStateOf<String?>(null) }
 
     val context = LocalContext.current
@@ -104,12 +80,10 @@ fun HomeScreen(
     val currentUser = FirebaseAuth.getInstance().currentUser
 
 
-    MyScaffold(
+    MyHomeScaffold (
         titleText = "Memories Book",
         navController = navController,
         context = context,
-        navigationContent = {
-        },
         actionIconContent = {
             IconButton(onClick = { navController.popBackStack() }) {
                 Icon(
@@ -127,6 +101,7 @@ fun HomeScreen(
             items(items = postList) { post ->
                 PostItem(
                     post = post,
+                    contentScale = contentScale,
                     onMenuClick = {
                         selectedPost = post
                         scope.launch { optionsSheetState.show() }
@@ -147,6 +122,13 @@ fun HomeScreen(
                         navController.navigate("media_viewer/${clickedPost.documentId}/${startIndex}")
                     }, onVideoFullScreen = { videoUrl ->
                         fullScreenVideoUrl = videoUrl
+                    },onAuthorClick = { authorId ->
+                        val route = if (authorId == currentUser?.uid) {
+                            "profile_screen/me"
+                        } else {
+                            "profile_screen/$authorId"
+                        }
+                        navController.navigate(route)
                     }
                 )
             }
