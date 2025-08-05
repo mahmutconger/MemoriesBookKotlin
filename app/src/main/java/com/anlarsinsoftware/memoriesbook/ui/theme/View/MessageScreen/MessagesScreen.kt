a006f8df9d63678063852e3afc45a0f39911dbf6
Mahmut Can Conger <mahmutconger@gmail.com>
2025-08-05 15:52:12 +0300

    myScaffold composable func used

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/MessageScreen/MessagesScreen.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/View/MessageScreen/MessagesScreen.kt
@@ -1,6 +1,5 @@
 package com.anlarsinsoftware.memoriesbook.ui.theme.View.MessageScreen
 
-import MessagesViewModel
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,6 +16,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,8 +29,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,107 +40,73 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.anlarsinsoftware.memoriesbook.R
-import com.anlarsinsoftware.memoriesbook.ui.theme.Model.FriendProfile
-import com.anlarsinsoftware.memoriesbook.ui.theme.Tools.BottomNavigationBar
-import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.ConnectionsViewModel
+import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Users
+import com.anlarsinsoftware.memoriesbook.ui.theme.Util.MyScaffold
+import com.anlarsinsoftware.memoriesbook.ui.theme.ViewModel.UserViewModel
+import com.google.firebase.Firebase
 import com.google.firebase.functions.FirebaseFunctions
-import com.google.firebase.functions.ktx.functions
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.functions.functions
 
 
 private lateinit var functions: FirebaseFunctions
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MessagesScreen(navController: NavController,
-                   connectionsViewModel: ConnectionsViewModel=viewModel()) {
+fun MessagesScreen(
+    navController: NavController,
+    userViewModel: UserViewModel
+) {
     functions = Firebase.functions
     val context = LocalContext.current
-    val friends by connectionsViewModel.friends.collectAsState()
+    val uiState by userViewModel.uiState.collectAsState()
+    val friends = uiState.myFriends
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        "Sohbetler",
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.popBackStack()
-                    }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            tint = MaterialTheme.colorScheme.primary,
-                            contentDescription = "Geri Butonu"
-                        )
-                    }
-                },
-
-                actions = {
-                    IconButton(onClick = {
-                      navController.navigate("connections_screen")
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            tint = MaterialTheme.colorScheme.primary,
-                            contentDescription = "Geri Butonu"
-                        )
-                    }
-                },
-
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Transparent
+    MyScaffold(
+        titleText = "Sohbetler",
+        navController = navController,
+        context = context,
+        actionIconContent = {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = "Menu ikonu"
                 )
-            )
-        }, bottomBar = {
-            BottomNavigationBar(
-                context = context,
-                createPostClick = {
-                    navController.navigate("createPost_screen")
-                }, profileClick = {
-                    navController.navigate("profile_screen")
-                }, messageClick = {
-
-                }, homeClick = {
-                    navController.navigate("home_screen")
-                })
-        }
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-
-            LazyColumn(
-                modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                items(items = friends) { friend ->
-                    FriendItem(
-                        friend = friend,
-                        onClick = {
-                            navController.navigate("chat_screen/${friend.uid}")
-                        }
-                    )
-                }
             }
+        },
+        navigationContent = {
 
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = "Geri dön"
+                )
+            }
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(items = friends) { friend ->
+                FriendItem(
+                    friend = friend,
+                    onClick = {
+                        navController.navigate("chat_screen/${friend.uid}")
+                    }
+                )
+            }
         }
     }
 }
+
 @Composable
-fun FriendItem(friend: FriendProfile, onClick: () -> Unit) {
+fun FriendItem(friend: Users, onClick: () -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -155,15 +119,12 @@ fun FriendItem(friend: FriendProfile, onClick: () -> Unit) {
             contentDescription = "${friend.username} profil fotoğrafı",
             error = painterResource(id = R.drawable.default_user),
             placeholder = painterResource(id = R.drawable.default_user),
-            modifier = Modifier.size(50.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
         )
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(8.dp))
+
         Text(friend.username, fontWeight = FontWeight.Bold, fontSize = 18.sp)
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun mes_prev() {
-    MessagesScreen(rememberNavController())
 }
