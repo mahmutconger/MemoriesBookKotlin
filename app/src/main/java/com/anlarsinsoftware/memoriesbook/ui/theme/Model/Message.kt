ddd4c1e2eb93397d275512149d782acc7fc6784f
Mahmut Can Conger <mahmutconger@gmail.com>
2025-08-07 12:19:29 +0300

    message type and reply parameters added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Model/Message.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Model/Message.kt
@@ -4,9 +4,14 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ServerTimestamp
 
 data class Message(
+    val replyToMessageId: String = "",
+    val replyToMessageText: String = "",
+    val replyToSenderUsername: String = "",
     val messageId: String = "",
     val senderId: String = "",
     val text: String = "",
+    val type: String = "text",
+    val mediaUrl: String? = null,
     @ServerTimestamp
     val timestamp: Timestamp = Timestamp.now()
 )
