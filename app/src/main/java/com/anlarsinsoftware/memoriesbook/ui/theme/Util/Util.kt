da2d94181c355f003ec28a62810fce49d5d4bbcb
Mahmut Can Conger <mahmutconger@gmail.com>
2025-08-07 12:18:42 +0300

    KISS_EMOJI_GIF const added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Util/Util.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/Util/Util.kt
@@ -1,3 +1,4 @@
 package com.anlarsinsoftware.memoriesbook.ui.theme.Util
 
 val url1="https://firebasestorage.googleapis.com/v0/b/englishwordapp-7fb3b.firebasestorage.app/o/gorsel%2F12fb5b13-5eaa-44c4-a9f2-df7def4dba05.jpg?alt=media&token=11239fac-d521-4863-bbaf-5afedc313b4e"
+val KISS_EMOJI_GIF="https://media.tenor.com/P26JRt3l2U4AAAAj/face-blowing-a-kiss-emoji.gif"
