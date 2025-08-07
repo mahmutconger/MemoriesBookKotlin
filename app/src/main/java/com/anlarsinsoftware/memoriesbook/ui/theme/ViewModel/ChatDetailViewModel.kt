677e7e53fdc7bb814c1759df72c2796de8ee42ff
Mahmut Can Conger <mahmutconger@gmail.com>
2025-08-07 12:17:03 +0300

    Message Reply added

--- app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/ViewModel/ChatDetailViewModel.kt
+++ app/src/main/java/com/anlarsinsoftware/memoriesbook/ui/theme/ViewModel/ChatDetailViewModel.kt
@@ -5,9 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.anlarsinsoftware.memoriesbook.ui.theme.Model.FriendProfile
+import com.anlarsinsoftware.memoriesbook.R
 import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Message
 import com.anlarsinsoftware.memoriesbook.ui.theme.Model.UserStatus
+import com.anlarsinsoftware.memoriesbook.ui.theme.Model.Users
+import com.anlarsinsoftware.memoriesbook.ui.theme.Util.KISS_EMOJI_GIF
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.Query
@@ -30,6 +32,10 @@ class ChatDetailViewModel(
     private val _userStatus = MutableStateFlow(UserStatus())
     val userStatus: StateFlow<UserStatus> = _userStatus.asStateFlow()
 
+    private val _replyingToMessage = MutableStateFlow<Message?>(null)
+    val replyingToMessage: StateFlow<Message?> = _replyingToMessage.asStateFlow()
+
+
     private fun observeStatus() {
         viewModelScope.launch {
             homeViewModel.observeUserStatus(friendId).collect { status ->
@@ -38,8 +44,8 @@ class ChatDetailViewModel(
         }
     }
 
-    private val _friendProfile = MutableStateFlow<FriendProfile?>(null)
-    val friendProfile: StateFlow<FriendProfile?> = _friendProfile.asStateFlow()
+    private val _friendProfile = MutableStateFlow<Users?>(null)
+    val friendProfile: StateFlow<Users?> = _friendProfile.asStateFlow()
 
     private val _messages = MutableStateFlow<List<Message>>(emptyList())
     val messages: StateFlow<List<Message>> = _messages.asStateFlow()
@@ -55,11 +61,23 @@ class ChatDetailViewModel(
     private fun loadFriendProfile() {
         firestore.collection("Users").document(friendId).get()
             .addOnSuccessListener { document ->
-                _friendProfile.value = document.toObject(FriendProfile::class.java)
+                _friendProfile.value = document.toObject(Users::class.java)
             }
     }
 
+    fun onStartReply(message: Message) {
+        _replyingToMessage.value = message
+    }
+
+    fun onCancelReply() {
+        _replyingToMessage.value = null
+    }
+
+
+
     fun sendMessage(text: String) {
+        val messageToReplyTo = _replyingToMessage.value
+
         val currentUser = auth.currentUser
         if (currentUser == null || text.isBlank()) {
             return
@@ -68,13 +86,44 @@ class ChatDetailViewModel(
         val chatRoomId = getChatRoomId(currentUser.uid, friendId)
         val messageRef = firestore.collection("chats").document(chatRoomId).collection("messages")
 
-        val message = Message(
-            senderId = currentUser.uid,
-            text = text
-        )
+        val message: Message
+
+        val replyToUsername = if (messageToReplyTo != null) {
+            // Eğer cevaplanan mesajı gönderen kişi mevcut kullanıcı ise, onun adını al.
+            if (messageToReplyTo.senderId == currentUser.uid) {
+                currentUser.displayName
+            } else {
+                _friendProfile.value?.username
+            }
+        } else {
+            ""
+        }
+
+        if (text.trim() == "😘") {
+            message = Message(
+                senderId = currentUser.uid,
+                type = "gif",
+                mediaUrl = KISS_EMOJI_GIF,
+                text = "😘",
+                replyToMessageId = messageToReplyTo?.messageId ?: "",
+                replyToMessageText = messageToReplyTo?.text ?: "",
+                replyToSenderUsername = replyToUsername ?: "",
+            )
+        } else {
+            message = Message(
+                replyToMessageId = messageToReplyTo?.messageId ?: "",
+                replyToMessageText = messageToReplyTo?.text ?: "",
+                replyToSenderUsername = replyToUsername ?: "",
+                senderId = currentUser.uid,
+                type = "text",
+                text = text
+            )
+        }
+
         messageRef.add(message)
             .addOnSuccessListener {
                 Log.d("SendMessage", "Message sent successfully!")
+                onCancelReply()
             }
             .addOnFailureListener { e ->
                 Log.e("SendMessage", "Error sending message", e)
